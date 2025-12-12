2d113e8759abbc59b6c764ed7c3f38f7735c5fde
Senja Erlandsson <senjaerlandsson@gmail.com>
2025-12-12 14:42:44 +0100

    feat admin middleware and update admin login

--- backend/admin/login.mjs
+++ backend/admin/login.mjs
@@ -1,58 +1,69 @@
 import "dotenv/config";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import middy from "@middy/core";
+import jsonBodyParser from "@middy/http-json-body-parser";
+import httpErrorHandler from "@middy/http-error-handler";
+
 import { responseHandler } from "../build/services/response-handler.mjs";
 import { GetItemCommand } from "@aws-sdk/client-dynamodb";
 import { client as db } from "../build/services/db.mjs";
 
+export const loginHandler = async (event) => {
+  const { adminId, password } = event.body;
 
-export const handler = async (event) => {
-  try {
-    const body = JSON.parse(event.body || "{}");
-
-    const { adminId, password } = body;
-
-    if (!adminId || !password) {
-      return responseHandler(400, { message: "Missing adminId or password" });
-    }
-
-    const getRes = await db.responseHandler(
-      new GetItemCommand({
-        TableName: process.env.ADMIN_TABLE,
-        Key: {
-          PK: { S: "ADMIN#root" }
-        }
-      })
-    );
-
-    if (!getRes.Item) {
-      console.error("No admin found in table!");
-      return responseHandler(500, { message: "Admin not found" });
-    }
-
-    const storedId = getRes.Item.adminId.S;
-    const storedHash = getRes.Item.passwordHash.S;
-
-    if (adminId !== storedId) {
-      return responseHandler(401);
-    }
-
-    const isValidPassword = await bcrypt.compare(password, storedHash);
-
-    if (!isValidPassword) {
-      return responseHandler(401);
-    }
-
-    const token = jwt.sign(
-      { adminId, role: "admin" },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
-
-    return responseHandler(200, { token });
-
-  } catch (err) {
-    console.error("Login error:", err);
-    return responseHandler(500);
+  if (!adminId || !password) {
+    return responseHandler(400, { message: "Missing adminId or password" });
   }
+
+  const getRes = await db.send(
+    new GetItemCommand({
+      TableName: process.env.ADMIN_TABLE,
+      Key: {
+        PK: { S: "ADMIN#root" }
+      }
+    })
+  );
+
+  if (!getRes.Item) {
+    console.error("No admin found in table!");
+    return responseHandler(500, { message: "Admin not found" });
+  }
+
+  const storedId = getRes.Item.adminId.S;
+  const storedHash = getRes.Item.passwordHash.S;
+
+  if (adminId !== storedId) {
+    return responseHandler(401);
+  }
+
+  const isValidPassword = await bcrypt.compare(password, storedHash);
+
+  if (!isValidPassword) {
+    return responseHandler(401);
+  }
+
+  const token = jwt.sign(
+    { adminId, role: "admin" },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+
+  const isProd = process.env.NODE_ENV === "production";
+  const isHttps = event.headers?.["x-forwarded-proto"] === "https";
+  const secureFlag = (isProd || isHttps) ? "; Secure" : "";
+
+  const cookie = `token=${token}; HttpOnly; Path=/; Max-Age=3600; SameSite=Lax${secureFlag}`;
+
+  /* Secure; läggs till vid produktion tyyyp*/
+
+  return responseHandler(
+    200,
+    { message: "Login successful" },
+    { "Set-Cookie": cookie }
+  );
 };
+
+export const handler = middy(loginHandler)
+  .use(jsonBodyParser())
+  .use(httpErrorHandler());
