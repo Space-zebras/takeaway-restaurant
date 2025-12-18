9e1d5f6049a358f8da475407ac3bf380cd98f0b8
Dennis Collins <donncha.ocoileain@hotmail.com>
2025-12-18 13:44:44 +0100

    Created admin stock page, stock API and useGetStock hook.

--- frontend/packages/pages/admin/stock-page/ui/index.tsx
+++ frontend/packages/pages/admin/stock-page/ui/index.tsx
@@ -1,3 +1,34 @@
+import { Container } from "@app/base";
+import { AdminStockItem } from "../children/admin-stockitem";
+import "./index.css";
+
 export function AdminStockPage() {
-  return <h1>Admin - Stock Page</h1>;
+  // Mock data (backend hook later)
+  const stock = [
+    { stockItem: "Beans", quantity: 30 },
+    { stockItem: "Cheese", quantity: 12 },
+    { stockItem: "Tortilla", quantity: 50 },
+    { stockItem: "Rice", quantity: 65 },
+  ];
+
+  return (
+    <main className="adminStockPage">
+      <Container title="Stock Levels" variant="full">
+        <div className="adminStockPage__header">
+          <span>Item</span>
+          <span>Portions available</span>
+        </div>
+
+        <div className="adminStockPage__list">
+          {stock.map((item) => (
+            <AdminStockItem
+              key={item.stockItem}
+              name={item.stockItem}
+              quantity={item.quantity}
+            />
+          ))}
+        </div>
+      </Container>
+    </main>
+  );
 }
